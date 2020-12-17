471a09a425c0a680d59d3daff4a4a4252211376d
hms5232 <hms5232@yahoo.com.tw>
2020-12-17 21:13:11 +0800

    +x and print info

--- install.sh
+++ install.sh
@@ -7,8 +7,10 @@
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
 sudo apt install mariadb-server nginx php7.4-fpm php7.4-mysql
+
 # https://tw.wordpress.org/download/
 wget https://tw.wordpress.org/latest-zh_TW.tar.gz
 tar -xvf latest-zh_TW.tar.gz
 sudo mv wordpress /var/www
+echo "Give permission to www-data."
 sudo chown -R www-data /var/www/wordpress/
