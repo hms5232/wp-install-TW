6dab1bf9777ea92e938183435e17fdf60552a237
hms5232 <43672033+hms5232@users.noreply.github.com>
2020-12-22 10:45:16 +0800

    修一些小地方和預備指令
    網址修正、增加輸出訊息
    另外增加 php7.4-dev 的預備指令，如果有需要的人直接取消註解即可

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 
-# Author: hms5232 ( https://gitlab.com/hms5232/wp-install-tw )
+# Author: hms5232 ( https://github.com/hms5232/wp-install-TW )
 
 
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
 sudo apt install mariadb-server php7.4-fpm php7.4-mysql php7.4-xml php7.4-curl php7.4-common php7.4-zip php7.4-mbstring php7.4-imagick php7.4-gd nginx
+# If you need WP-CLI, you shall uncomment the following line
+#sudo apt install php7.4-dev
 
 # https://tw.wordpress.org/download/
 wget https://tw.wordpress.org/latest-zh_TW.tar.gz
@@ -14,3 +16,5 @@ tar -xvf latest-zh_TW.tar.gz
 sudo mv wordpress /var/www
 echo "Give permission to www-data."
 sudo chown -R www-data /var/www/wordpress/
+
+echo "｡:.ﾟヽ(*´∀`)ﾉﾟ.:｡ Done!"
